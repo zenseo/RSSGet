0879919e629184a5f00fd80351cc4ec9e236b4a8
Aleksandr Zelenin <aleksandr@zelenin.me>
2013-04-02 01:07:04 +0400

    version 1.0.2
    sanitize of input feed

--- lib/Zelenin/RSSGet.php
+++ lib/Zelenin/RSSGet.php
@@ -2,10 +2,11 @@
 
 /**
  * PHP class for parsing of RSS-feeds and atoms. Output to array.
+ *
  * @package RSSGet
  * @author  Aleksandr Zelenin <aleksandr@zelenin.me>
  * @link    https://github.com/zelenin/rssget
- * @version 1.0.1
+ * @version 1.0.2
  * @license http://opensource.org/licenses/gpl-3.0.html GPL-3.0
  */
 
@@ -31,9 +32,11 @@ class RSSGet extends DOMDocument
 		$this->feed_url = $feed_url;
 		$feed = $curl->get( $this->feed_url );
 		$this->http_code = $feed['info']['http_code'];
-		if ( $this->http_code != 200 ) return false;
+		if ( $this->http_code != 200 ) {
+			return false;
+		}
 
-		$this->feed_content = $feed['body'];
+		$this->feed_content = $this->sanitizeString( $feed['body'] );
 		$this->loadXML( $this->feed_content );
 		$this->check_type();
 		$this->get_channel();
@@ -58,12 +61,13 @@ class RSSGet extends DOMDocument
 		$channel = $this->getElementsByTagName( $this->channel_tag )->item( 0 );
 		$channel_elements = $channel->getElementsByTagName( '*' );
 		for ( $i = 0; $i < $channel_elements->length; $i++ ) {
-			if ( $channel_elements->item( $i )->nodeName == $this->item_tag ) break;
-			$this->channel[$channel_elements->item( $i )->nodeName] = $this->string_normalize( $channel_elements->item( $i )->nodeValue );
+			if ( $channel_elements->item( $i )->nodeName == $this->item_tag ) {
+				break;
+			}
+			$this->channel[$channel_elements->item( $i )->nodeName] = $this->normalizeString( $channel_elements->item( $i )->nodeValue );
 			if ( $channel_elements->item( $i )->hasAttributes() ) {
-				$attributes = $channel_elements->item( $i )->attributes;
 				foreach ( $channel_elements->item( $i )->attributes as $attribute ) {
-					$this->channel[$channel_elements->item( $i )->nodeName . '_' . $attribute->name] = $this->string_normalize( $attribute->value );
+					$this->channel[$channel_elements->item( $i )->nodeName . '_' . $attribute->name] = $this->normalizeString( $attribute->value );
 				}
 			}
 		}
@@ -76,11 +80,10 @@ class RSSGet extends DOMDocument
 		for ( $i = 0; $i < $elements->length; $i++ ) {
 			$item_elements = $elements->item( $i )->getElementsByTagName( '*' );
 			for ( $j = 0; $j < $item_elements->length; $j++ ) {
-				$this->items[$i][$item_elements->item( $j )->nodeName] = $this->string_normalize( $item_elements->item( $j )->nodeValue );
+				$this->items[$i][$item_elements->item( $j )->nodeName] = $this->normalizeString( $item_elements->item( $j )->nodeValue );
 				if ( $item_elements->item( $j )->hasAttributes() ) {
-					$attributes = $item_elements->item( $j )->attributes;
 					foreach ( $item_elements->item( $j )->attributes as $attribute ) {
-						$this->items[$i][$item_elements->item( $j )->nodeName . '_' . $attribute->name] = $this->string_normalize( $attribute->value );
+						$this->items[$i][$item_elements->item( $j )->nodeName . '_' . $attribute->name] = $this->normalizeString( $attribute->value );
 					}
 				}
 			}
@@ -88,12 +91,17 @@ class RSSGet extends DOMDocument
 		return $this->items;
 	}
 
-	private function string_normalize( $string )
+	private function normalizeString( $string )
 	{
 		$string = htmlspecialchars( $string, ENT_QUOTES, 'utf-8', false );
 		// $string = str_replace(  '&amp;', '&#x26;', $string );
 		// $string = str_replace(  '&lt;', '&#x3C;', $string );
 		// $string = str_replace(  '&gt;', '&#x3E;', $string );
 		return $string;
+	}
+
+	private function sanitizeString( $string )
+	{
+		return preg_replace( '/[\x00-\x08\x0E-\x1F]/', '', $string );
 	}
 }
